07f517b858aa83190596c2532603eca8c5c677b3
Alfonso Ríos <manuelalfonso.rios.medellin.next@bbva.com>
2019-02-25 15:03:00 -0600

    refactor(*): Replace google-maps for other wrapper

--- map-component.js
+++ map-component.js
@@ -1,6 +1,5 @@
 import {html, css, LitElement} from 'lit-element';
-import '@em-polymer/google-map/google-map.js'
-import '@em-polymer/google-map/google-map-marker.js'
+import "google-maps-limited/google-maps-limited.js";
 
 class MapComponent extends LitElement {
     static get styles() {
@@ -10,19 +9,43 @@ class MapComponent extends LitElement {
 
     static get properties() {
         return {
-            apiKey:{type: String} 
+            apiKey:{type: String} ,
+            markers: {type: Array}
         }
     }
 
     constructor() {
         super();
         this.apiKey = 'AIzaSyBgmI1bWkGcebNPjwZpxSpCQ2yCWDGSJ1I';
+        this.markers = [
+            {
+              position: {lat:41, lng:-112},
+              InfoWindowContent: "<h3>The Salt Lake City, UT Temple</h3>"
+            },
+            {
+              position: {lat:33, lng:-117},
+              InfoWindowContent: "<h3>The San Diego, CA Temple</h3>"
+            },
+            {
+              position: {lat:29, lng:-82},
+              InfoWindowContent: "<h3>The Orlando, FL Temple</h3>"
+            },
+            {
+              position: {lat:43, lng:-70},
+              InfoWindowContent: "<h3>The Cape Elizabeth, ME Temple</h3>"
+            }
+          ];
     }
 
     render(){
         return html`
             <h1>Map</h1>
-            <google-map api-key="${this.apiKey}"></google-map>
+            <google-maps-limited 
+                style="height:65vh;width:80vw" 
+                selectLocationMode 
+                .apiKey="${this.apiKey}" 
+                .markers="${this.markers}">
+            </google-maps-limited>
         `
     }
 }
